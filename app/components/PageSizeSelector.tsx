392cab7598443fce91f7728ab7495fbfdcad327c
Nemanja <bogojevicn5199@gmail.com>
2024-12-31 02:14:59 +0100

    Small fix

--- app/components/PageSizeSelector.tsx
+++ app/components/PageSizeSelector.tsx
@@ -30,8 +30,8 @@ export const PageSizeSelector = ({ currentSize }: PageSizeSelectorProps) => {
         onChange={handleSizeChange}
       >
         <option value="5">5</option>
-        <option value="15">15</option>
         <option value="10">10</option>
+        <option value="15">15</option>
         <option value="20">20</option>
         <option value="50">50</option>
       </select>
